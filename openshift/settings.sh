07b0b5bc9cf7db891743a1ec67439198a52ad99b
Curtis Upshall <curtisupshall@gmail.com>
2022-07-11 13:06:09 -0700

    Create openshift folder

---
+++ openshift/settings.sh
@@ -0,0 +1,12 @@
+export PROJECT_NAMESPACE="issuer-kit-demo"
+export GIT_URI="https://github.com/curtisupshall/curtis-world.git"
+export GIT_REF="master"
+export SKIP_PIPELINE_PROCESSING=1
+
+export include_templates="curtis-world"
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Override the TEST, PROD deployment environment
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export TEST="dev"
+export PROD="dev"
